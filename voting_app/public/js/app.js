0bc60b1a27944c93fd301172fa6291d4460ceb18
dave doolin <david.doolin@gmail.com>
2020-06-04 04:49:06 -0700

    28 rendering app-1 in browser

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,1 +1,15 @@
 // Restart the book on p. 20
+class ProductList extends React.Component {
+  render () {
+    return (
+      <div className='ui unstackable items'>
+        Hello, friend! I am a basic React Component.
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <ProductList />,
+  document.getElementById('content')
+);
