237bc311e56427be2df982593a52d44791ef63c7
dave doolin <david.doolin@gmail.com>
2020-06-05 06:18:30 -0700

    32 product to product list

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,9 +1,32 @@
-// Restart the book on p. 20
 class ProductList extends React.Component {
   render () {
     return (
       <div className='ui unstackable items'>
-        Hello, friend! I am a basic React Component.
+        <Product />
+      </div>
+    )
+  }
+}
+class Product extends React.Component {
+  render () {
+    return (
+      <div className='item'>
+        <div className='image'>
+          <img src='images/products/image-aqua.png' />
+        </div>
+        <div className='middle aligned content'>
+          <div className='desxcription'>
+            <a>Fort Knight</a>
+            <p>Authentic Renaissance actors, delivered in just two weeks.</p>
+          </div>
+          <div className='extra'>
+            <span>Submitted by:</span>
+            <img
+              className='ui avatar image'
+              src='images/avatars/daniel.jpg'
+            />
+          </div>
+        </div>
       </div>
     );
   }
