cea2e1f4277e7dc3a95b0c71aba7d82939a37c1d
dave doolin <david.doolin@gmail.com>
2020-06-12 04:38:16 -0700

    68/91 use babel-style binding

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,19 +1,15 @@
+/* eslint-disable no-param-reassign, operator-assignment */
+
 class ProductList extends React.Component {
-  constructor(props) {
-    super(props);
+  state = {
+    products: [],
+  };
 
-    this.state = {
-      products: [],
-    };
-
-    this.handleProductUpVote = this.handleProductUpVote.bind(this);
-  }
-  
   componentDidMount() {
     this.setState({ products: Seed.products });
   }
 
-  handleProductUpVote(productId) {
+  handleProductUpVote = (productId) => {
     const nextProducts = this.state.products.map((product) => {
       if (product.id === productId) {
         return Object.assign({}, product, {
@@ -55,15 +51,9 @@ class ProductList extends React.Component {
   }
 }
 class Product extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleUpVote = this.handleUpVote.bind(this);
-  }
-
-  handleUpVote () {
-    this.props.onVote(this.props.id);
-  }
+  handleUpVote = () => (
+    this.props.onVote(this.props.id)
+  );
 
   render () {
     return (
