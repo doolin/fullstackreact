778c9c671445db13e0fdebd1c9ad80383dbbb3f3
dave doolin <david.doolin@gmail.com>
2020-06-15 04:12:56 -0700

    88/111 add the timers

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -10,3 +10,27 @@ class TimersDashboard extends React.Component {
     );
   }
 }
+
+class EditableTimerList extends React.Component {
+  render() {
+    return (
+      <div id='timers'>
+        <EditableTimer
+          title='Learn React'
+          project='Web Domination'
+          elapsed='8986300'
+          runningSince={null}
+          editFormOpen={false}
+        />
+      <div id='timers'>
+        <EditableTimer
+          title='Learn extreme ironing'
+          project='Web Domination'
+          elapsed='3890985'
+          runningSince={null}
+          editFormOpen={true}
+        />
+      </div>
+    );
+  }
+}
