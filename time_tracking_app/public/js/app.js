77e1733b2b05159aa7db04e1a59385aa8771d758
dave doolin <david.doolin@gmail.com>
2020-06-19 04:40:41 -0700

    93/116 timer

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -1,4 +1,5 @@
 const { extname } = require("path");
+const e = require("express");
 
 class TimersDashboard extends React.Component {
   render() {
@@ -104,5 +105,37 @@ class ToggleableTimerForm extends React.Component {
         </div>
       );
     }
+  }
+}
+
+class Timer extends React.Component {
+  render() {
+    const elapsedString = helpers.renderElapsedString(this.props.elapsed);
+
+    return (
+      <div className='ui centered card'>
+        <div className='content'>
+          <div className='header'>
+            {this.props.title}
+          </div>
+          <div className='meta'>
+            {this.props.project}
+          </div>
+          <div className='center aligned description'>
+            <h2>
+              {elapsedString}
+            </h2>
+          </div>
+          <div className='extra content'>
+            <span className='right floated edit icon'>
+              <i className='edit icon' />
+            </span>
+          </div>
+        </div>
+        <div className='ui bottom attached blue basic button'>
+          Start
+        </div>
+      </div>
+    );
   }
 }
