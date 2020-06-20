8b43b517c7f8a98abaa34413f7a09c4a80dfeb19
dave doolin <david.doolin@gmail.com>
2020-06-20 05:14:38 -0700

    94/117 render react dom

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -1,6 +1,7 @@
-const { extname } = require("path");
-const e = require("express");
-
+/*
+  eslint-disable react/prefer-stateless-function, react/jsx-boolean-value,
+  no-undef, jsx-a11y/label-has-for
+*/
 class TimersDashboard extends React.Component {
   render() {
     return (
@@ -17,18 +18,18 @@ class TimersDashboard extends React.Component {
 class EditableTimerList extends React.Component {
   render() {
     return (
-      <div id='timers'>
+      <div id="timers">
         <EditableTimer
-          title='Learn React'
-          project='Web Domination'
-          elapsed='8986300'
+          title="Learn React"
+          project="Web Domination"
+          elapsed="8986300"
           runningSince={null}
           editFormOpen={false}
         />
         <EditableTimer
-          title='Learn extreme ironing'
-          project='Web Domination'
-          elapsed='3890985'
+          title="Learn extreme ironing"
+          project="Web Domination"
+          elapsed="3890985"
           runningSince={null}
           editFormOpen={true}
         />
@@ -41,10 +42,7 @@ class EditableTimer extends React.Component {
   render() {
     if (this.props.editFormOpen) {
       return (
-        <TimerForm
-          title={this.props.title}
-          project={this.props.project}
-        />
+        <TimerForm title={this.props.title} project={this.props.project} />
       );
     } else {
       return (
@@ -64,24 +62,20 @@ class TimerForm extends React.Component {
     const submitText = this.props.title ? 'Update' : 'Create';
 
     return (
-      <div className='ui centered card'>
-        <div className='content'>
-          <div className='ui form'>
-            <div className='field'>
+      <div className="ui centered card">
+        <div className="content">
+          <div className="ui form">
+            <div className="field">
               <label>Title</label>
-              <input type='text' defaultValue={this.props.title} />
+              <input type="text" defaultValue={this.props.title} />
             </div>
-            <div className='field'>
+            <div className="field">
               <label>Project</label>
-              <input type='text' defaultValue={this.props.project} />
+              <input type="text" defaultValue={this.props.project} />
             </div>
-            <div className='ui two bottom attached buttons'>
-              <button className='ui basic blue button'>
-                {submitText}
-              </button>
-              <button className='ui basic red button'>
-                Cancel
-              </button>
+            <div className="ui two bottom attached buttons">
+              <button className="ui basic blue button">{submitText}</button>
+              <button className="ui basic red button">Cancel</button>
             </div>
           </div>
         </div>
@@ -93,14 +87,12 @@ class TimerForm extends React.Component {
 class ToggleableTimerForm extends React.Component {
   render() {
     if (this.props.isOpen) {
-      return (
-        <TimerForm />
-      );
+      return <TimerForm />;
     } else {
       return (
-        <div className='ui basic content center aligned segment'>
-          <button className='ui basic button icon'>
-            <i className='plus icon' />
+        <div className="ui basic content center aligned segment">
+          <button className="ui basic button icon">
+            <i className="plus icon" />
           </button>
         </div>
       );
@@ -113,29 +105,23 @@ class Timer extends React.Component {
     const elapsedString = helpers.renderElapsedString(this.props.elapsed);
 
     return (
-      <div className='ui centered card'>
-        <div className='content'>
-          <div className='header'>
-            {this.props.title}
+      <div className="ui centered card">
+        <div className="content">
+          <div className="header">{this.props.title}</div>
+          <div className="meta">{this.props.project}</div>
+          <div className="center aligned description">
+            <h2>{elapsedString}</h2>
           </div>
-          <div className='meta'>
-            {this.props.project}
-          </div>
-          <div className='center aligned description'>
-            <h2>
-              {elapsedString}
-            </h2>
-          </div>
-          <div className='extra content'>
-            <span className='right floated edit icon'>
-              <i className='edit icon' />
+          <div className="extra content">
+            <span className="right floated edit icon">
+              <i className="edit icon" />
             </span>
           </div>
         </div>
-        <div className='ui bottom attached blue basic button'>
-          Start
-        </div>
+        <div className="ui bottom attached blue basic button">Start</div>
       </div>
     );
   }
 }
+
+ReactDOM.render(<TimersDashboard />, document.getElementById('content'));
