f5cdc25f331aac6bda55e48bac5624250af68388
dave doolin <david.doolin@gmail.com>
2020-07-19 05:35:38 -0700

    142/179 start of persistence

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -3,25 +3,29 @@
   no-undef, jsx-a11y/label-has-for
 */
 class TimersDashboard extends React.Component {
-  state = {
-    timers: [
-      {
-        title: 'Practice squat',
-        project: 'Gym Chores',
-        id: uuid.v4(),
-        elapsed: 5456099,
-        runningSince: Date.now(),
-      },
-      {
-        title: 'Bake squash',
-        project: 'Kitchen Chores',
-        id: uuid.v4(),
-        elapsed: 1273998,
-        runningSince: null,
-      },
-    ],
-  };
+  // state = {
+  //   timers: [
+  //     {
+  //       title: 'Practice squat',
+  //       project: 'Gym Chores',
+  //       id: uuid.v4(),
+  //       elapsed: 5456099,
+  //       runningSince: Date.now(),
+  //     },
+  //     {
+  //       title: 'Bake squash',
+  //       project: 'Kitchen Chores',
+  //       id: uuid.v4(),
+  //       elapsed: 1273998,
+  //       runningSince: null,
+  //     },
+  //   ],
+  // };
 
+  state = {
+    timers: []
+  };
+  
   handleCreateFormSubmit = (timer) => {
     this.createTimer(timer);
   };
