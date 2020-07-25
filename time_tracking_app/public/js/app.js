01e18cfe31d3aea36721528bdac7f405d938594e
dave doolin <david.doolin@gmail.com>
2020-07-25 04:39:48 -0700

    161/184 remove duplicate start button

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -385,7 +385,6 @@ class Timer extends React.Component {
             </span>
           </div>
         </div>
-        <div className="ui bottom attached blue basic button">Start</div>
         <TimerActionButton
           timerIsRunning={!!this.props.runningSince}
           onStartClick={this.handleStartClick}
