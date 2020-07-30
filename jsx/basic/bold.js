5570584310e762ab7622c16ddaa065070769552d
dave doolin <david.doolin@gmail.com>
2020-07-30 04:21:18 -0700

    168/191 building DOM now from script
    Next is to display the class which is in the
    bold.js file.

--- jsx/basic/bold.js
+++ jsx/basic/bold.js
@@ -4,7 +4,17 @@ class Bold extends React.Component {
   }
 }
 
-var mountElement = document.getElementById('root');
-var boldElement = React.createElement('b');
-ReactDOM.render(boldElement, mountElement)
 // ReactDOM.render(React.createElement(Bold, {}), mountElement)
+
+// This works in the console, but does not work
+// when loaded from this file. It fails with
+// `Uncaught Invariant Violation: Target container is not a DOM element.`
+// Not sure what to do about it yet. I could read the React
+// source code for help.
+//
+// This script has to be run _after_ the DOM is loaded.
+// The problem above was that I was loading the script
+// in the header.
+var mountElement = document.getElementById('root');
+var boldElement = React.createElement('b', null, "Some text");
+ReactDOM.render(boldElement, mountElement);
--- jsx/basic/vendor/react-dom.js
+++ jsx/basic/vendor/react-dom.js
@@ -65,7 +65,7 @@ function invariant(condition, format, a, b, c, d, e, f) {
       error = new Error(format.replace(/%s/g, function () {
         return args[argIndex++];
       }));
-      error.name = 'Invariant Violation';
+      error.name = 'Invariant Violation booyah';
     }
 
     error.framesToPop = 1; // we don't care about invariant's own frame
