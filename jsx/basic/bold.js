9637f4b24e55d43fbd68f8a0eee07e48c11e2dab
dave doolin <david.doolin@gmail.com>
2020-08-01 03:57:58 -0700

    168/191 example of overwriting mount point
    The first example is overwritten by the second
    example since it's mounted at the same point in
    the document after the first example.

--- jsx/basic/bold.js
+++ jsx/basic/bold.js
@@ -1,21 +1,23 @@
+// This script has to be run _after_ the DOM is loaded.
+// The problem above was that I was loading the script
+// in the header.
+//
+// If it's loaded in the header, it works in the console,
+// but does not work when loaded from this file. It fails with
+// `Uncaught Invariant Violation: Target container is not a DOM element.`
+
+// index.html in this example
+var mountElement = document.getElementById('root');
+
+// Example 1 building from class works like so:
 class Bold extends React.Component {
   render () {
     return React.createElement('b', null, 'Text from Bold class');
   }
 }
-
-var mountElement = document.getElementById('root');
 ReactDOM.render(React.createElement(Bold, {}), mountElement)
 
-// This works in the console, but does not work
-// when loaded from this file. It fails with
-// `Uncaught Invariant Violation: Target container is not a DOM element.`
-// Not sure what to do about it yet. I could read the React
-// source code for help.
-//
-// This script has to be run _after_ the DOM is loaded.
-// The problem above was that I was loading the script
-// in the header.
-// var mountElement = document.getElementById('root');
-// var boldElement = React.createElement('b', null, "Some text");
-// ReactDOM.render(boldElement, mountElement);
+// Example 2 also works, but will overwrite the previously
+// mounted element from Example 1.
+var boldElement = React.createElement('b', null, "Some text");
+ReactDOM.render(boldElement, mountElement);
