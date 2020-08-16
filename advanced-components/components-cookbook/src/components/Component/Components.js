bcea23cdf3e4257479e41e4844e04ae167307107
dave doolin <david.doolin@gmail.com>
2020-08-16 04:41:40 -0700

    185/206 some annotations in provided source
    From yesterday, some investigation into babel configuration
    convinced me it's not something I want to deal with at the
    moment. There is a lot to it, and won't be learned in a
    session or two.
    
    This chapter will apparently be best worked on via the
    companion source code to the book. This commit provides
    a page number reference in the source to the relevant
    discussion in the book.
    
    Tomorrow: continue reading and annotating the source code.
    
    Also: small thunderstorm this morning. It's 75 degrees inside
    at 3:30 am.

--- advanced-components/components-cookbook/src/components/Component/Components.js
+++ advanced-components/components-cookbook/src/components/Component/Components.js
@@ -2,6 +2,8 @@ import React from "react";
 
 // ES6 class-style
 class ComponentApp extends React.Component {
+  // From r40, p. 162:
+  // render is the only required function in a ReactComponent.
   render() {
     return <div />;
   }
--- advanced-components/components-cookbook/src/components/Component/FunctionComponent.js
+++ advanced-components/components-cookbook/src/components/Component/FunctionComponent.js
@@ -1,4 +1,6 @@
 // Functional Component Style
+// From r40, p. 185: When using a function component,
+// the function itself is essentially the rnder() function.
 const FunctionComponent = props => {
   return <div />;
 };
